8af329c20921d54fb1ac344ef6461267c0c8bb35
Ömer Tokay <inborninnate1@gmail.com>
2024-06-07 14:30:34 +0300

    Employee Service

--- src/main/java/org/cydeo/service/employeeService.java
+++ src/main/java/org/cydeo/service/employeeService.java
@@ -1,4 +1,22 @@
 package org.cydeo.service;
 
+import org.cydeo.model.Employee;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class employeeService {
+
+    public static List<Employee> employeeList = new ArrayList<>();
+
+    public void saveEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
+
+    public List<Employee> readAllEmployees() {
+        return employeeList;
+    }
+
 }
